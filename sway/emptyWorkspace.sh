b87a015648d07cf3842de63ddd2bde54d4a613e6
fatualux <35587292+fatualux@users.noreply.github.com>
2023-12-15 09:45:32 +0100

    Reset

--- sway/emptyWorkspace.sh
+++
@@ -1,1 +0,0 @@
-swaymsg workspace $(($(i3-msg -t get_workspaces | tr , '\n' | grep '"num":' | cut -d : -f 2 | sort -rn | head -1) + 1))
--- waybar/custom/wfrecord-stop.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-pkill -SIGINT wf-recorder && dunstify ' screen cap ended'
-sleep .1
-
-# send signal to update monitor
-pkill -RTMIN+8 waybar

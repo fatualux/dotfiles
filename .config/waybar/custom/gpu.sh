978c9171c4c5b2a83dfc42b8fd8ddc4e93b8594b
fatualux <35587292+fatualux@users.noreply.github.com>
2023-12-25 22:47:36 +0100

    Edited gpu.sh to waybar's custom

--- .config/waybar/custom/gpu.sh
+++ .config/waybar/custom/gpu.sh
@@ -1,23 +1,35 @@
 #!/bin/bash
 
-# Get GPU load percentage
-GPU_LOAD=$(nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits)
+# Get GPU information
+GPU_NAME=$(nvidia-smi --query-gpu=name --format=csv,noheader,nounits)
+GPU_LOAD=$(nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits | sed 's/%//')
+GPU_TEMP=$(nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader,nounits | awk '{printf("%d°C", $1)}')
 
 # Define color thresholds
 LOW_LOAD_THRESHOLD=30
 MEDIUM_LOAD_THRESHOLD=60
 
 # Set default color
-COLOR="#00FF00"  # Green
+COLOR_GREEN="\e[92m"
+COLOR_ORANGE="\e[33m"
+COLOR_YELLOW="\e[93m"
+COLOR_RESET="\e[0m"
+
+# Initialize color variable
+COLOR=""
 
 # Check load range and update color accordingly
-if [ "$GPU_LOAD" -gt "$MEDIUM_LOAD_THRESHOLD" ]; then
-    COLOR="#FFA500"  # Orange
+if [ "${GPU_LOAD}" -gt "$MEDIUM_LOAD_THRESHOLD" ]; then
+    COLOR="${COLOR_ORANGE}"  # Orange
 fi
 
-if [ "$GPU_LOAD" -gt "$LOW_LOAD_THRESHOLD" ]; then
-    COLOR="#FFFF00"  # Yellow
+if [ "${GPU_LOAD}" -gt "$LOW_LOAD_THRESHOLD" ]; then
+    COLOR="${COLOR_YELLOW}"  # Yellow
 fi
 
-# Output the color
-echo "$COLOR"
+# Output the information with color if applicable
+if [ -n "${COLOR}" ]; then
+    echo -e "${COLOR}${GPU_NAME} - Load: ${GPU_LOAD}% - Temp: ${GPU_TEMP}${COLOR_RESET}"
+else
+    echo "${GPU_NAME} - Load: ${GPU_LOAD}% - Temp: ${GPU_TEMP}"
+fi

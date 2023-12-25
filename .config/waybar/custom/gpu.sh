1992526043a84bda854e4029344eb0b1e83ebae6
fatualux <35587292+fatualux@users.noreply.github.com>
2023-12-25 23:46:08 +0100

    Edited custom-gpu

--- .config/waybar/custom/gpu.sh
+++ .config/waybar/custom/gpu.sh
@@ -5,31 +5,15 @@ GPU_NAME=$(nvidia-smi --query-gpu=name --format=csv,noheader,nounits)
 GPU_LOAD=$(nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits | sed 's/%//')
 GPU_TEMP=$(nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader,nounits | awk '{printf("%d°C", $1)}')
 
-# Define color thresholds
-LOW_LOAD_THRESHOLD=30
-MEDIUM_LOAD_THRESHOLD=60
+# Set default class
+GPU_CLASS="custom-gpu"
 
-# Set default color
-COLOR_GREEN="\e[92m"
-COLOR_ORANGE="\e[33m"
-COLOR_YELLOW="\e[93m"
-COLOR_RESET="\e[0m"
-
-# Initialize color variable
-COLOR=""
-
-# Check load range and update color accordingly
-if [ "${GPU_LOAD}" -gt "$MEDIUM_LOAD_THRESHOLD" ]; then
-    COLOR="${COLOR_ORANGE}"  # Orange
+# Set class based on GPU load
+if [ "$GPU_LOAD" -ge 75 ]; then
+  GPU_CLASS="${GPU_CLASS}.critical"
+elif [ "$GPU_LOAD" -ge 50 ]; then
+  GPU_CLASS="${GPU_CLASS}.warning"
 fi
 
-if [ "${GPU_LOAD}" -gt "$LOW_LOAD_THRESHOLD" ]; then
-    COLOR="${COLOR_YELLOW}"  # Yellow
-fi
-
-# Output the information with color if applicable
-if [ -n "${COLOR}" ]; then
-    echo -e "${COLOR}${GPU_NAME} - Load: ${GPU_LOAD}% - Temp: ${GPU_TEMP}${COLOR_RESET}"
-else
-    echo "${GPU_NAME} - Load: ${GPU_LOAD}% - Temp: ${GPU_TEMP}"
-fi
+# Print the GPU information with class
+echo "$GPU_NAME - Load: $GPU_LOAD% - Temp: $GPU_TEMP"
